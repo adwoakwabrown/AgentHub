0c0dac0eb0d2f85dbe07b2021e080ee174284646
Ammar Rajihi <jennifersmith126@naoko.web.id>
2026-07-16 22:59:01 +0700

    Add main.go
    Co-authored-by: nancodero <nancodero@proton.me>

---
+++ cmd/agenthubx/main.go
@@ -0,0 +1,23 @@
+// cmd/agenthubx/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "agenthubx/internal/agenthubx"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := agenthubx.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
